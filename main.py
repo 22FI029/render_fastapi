9f7822c3f375be19ffbf26e9c05d326096a07ea1
22FI029 <166776246+22FI029@users.noreply.github.com>
2024-06-14 14:22:54 +0900

    barentain

--- main.py
+++ main.py
@@ -48,3 +48,7 @@ def index():
     </html>
     """
     return HTMLResponse(content=html_content, status_code=200)
+
+    @app.post("/present")
+async def new_naming(present):
+    return {"response": f"サーバです。ハッピーバレンタイン！ {present}ありがとう。お返しはDiorのリップです。"}
